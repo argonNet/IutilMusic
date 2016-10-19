62a97f9431148796048e0ad4fa38163a7a48bff1
Joelle Perritaz <joelle.perritaz@gmail.com>
2016-10-19 20:55:51 +0200

    Initial Commit

---
+++ IUtilMusic/IUtilMusic/App.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Leap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IUtilMusic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public const int KEYEVENTF_EXTENTEDKEY = 1;
+        public const int KEYEVENTF_KEYUP = 0;
+        public const int VK_VOLUME_MUTE = 0xAD; //Code to Mute sound
+        public const int VK_VOLUME_DOWN = 0xAE; //Code to volume down
+        public const int VK_VOLUME_UP = 0xAF; //Code to volume up
+        public const int VK_MEDIA_NEXT_TRACK = 0xB0;// code to jump to next track
+        public const int VK_MEDIA_STOP = 0xB2; //code to stop a song
+        public const int VK_MEDIA_PLAY_PAUSE = 0xB3;// code to play or pause a song
+        public const int VK_MEDIA_PREV_TRACK = 0xB1;// code to jump to prev 
+        
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private bool _isExit;
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+
+            _notifyIcon = new System.Windows.Forms.NotifyIcon();
+            _notifyIcon.DoubleClick += (s, args) => ShowMainWindow();
+            _notifyIcon.Icon = IUtilMusic.Properties.Resources.MyIcon;
+            _notifyIcon.Visible = true;
+
+            CreateContextMenu();
+
+            using (Controller controller = new Controller())
+            {
+                controller.FrameReady += newFrameHandler;
+            }
+        }
+
+        private void newFrameHandler(object sender, FrameEventArgs eventArgs)
+        {
+            Frame frame = eventArgs.frame;
+            //The following are Label controls added in design view for the form
+            Console.WriteLine(frame.Hands.Count);
+            //this.displayID.Text = frame.Id.ToString();
+            //this.displayTimestamp.Text = frame.Timestamp.ToString();
+            //this.displayFPS.Text = frame.CurrentFramesPerSecond.ToString();
+            //this.displayHandCount.Text = frame.Hands.Count.ToString();
+        }
+
+        private void CreateContextMenu()
+        {
+            _notifyIcon.ContextMenuStrip =
+              new System.Windows.Forms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("MainWindow...").Click += (s, e) => ShowMainWindow();
+            _notifyIcon.ContextMenuStrip.Items.Add("Mute/Unmute volume").Click += (s, e) => MuteVolume();
+            _notifyIcon.ContextMenuStrip.Items.Add("Play/Pause media").Click += (s, e) => PlayMedia();
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
+
+        }
+
+        private void MuteVolume()
+        {
+            keybd_event(VK_VOLUME_MUTE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        private void PlayMedia()
+        {
+            keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        private void ExitApplication()
+        {
+            _isExit = true;
+            MainWindow.Close();
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+
+        private void ShowMainWindow()
+        {
+            if (MainWindow.IsVisible)
+            {
+                if (MainWindow.WindowState == WindowState.Minimized)
+                {
+                    MainWindow.WindowState = WindowState.Normal;
+                }
+                MainWindow.Activate();
+            }
+            else
+            {
+                MainWindow.Show();
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!_isExit)
+            {
+                e.Cancel = true;
+                MainWindow.Hide(); // A hidden window can be shown again, a closed one not
+            }
+        }
+    }
+}
